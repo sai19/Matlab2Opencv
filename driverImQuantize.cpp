8ea4666bc89d38c302f9fe6c9ce52738a06a8d68
sai19 <s.koturwar@gmail.com>
2016-01-20 17:45:52 +0530

    Update driverImQuantize.cpp

--- driverImQuantize.cpp
+++ driverImQuantize.cpp
@@ -19,15 +19,19 @@ int main(int argc, char** argv)
 //define new image matrices
 Mat src;
 Mat dst;
+int type;
 // read the image (path is given through command line)
 src = imread(argv[1],1);
-// testcase for levels
+// define your own quantization levels
 int L[] = {123,150,198};
 // convert the image to graylevel 
 cvtColor( src,src, CV_BGR2GRAY );
 // apply imquantize on the image and store in dst
-dst = imQuantize(src,L,sizeof(L)/sizeof(*L),1);
+// if type=1; pixel values are replaced by interval no. (i.e (for choosen case) [0,123]=1;[123,150]=2;[150,198]=3;[198 and above]=4;)
+// if type=0; pixel values are replaced by actual quantization values
+dst = imQuantize(src,L,sizeof(L)/sizeof(*L),type);
 // output/display the image
+// for output with type 1 image may look blank but it is not(you can confirm it by printing out the matrix) as the assigned values will be pretty small on the scale of 255   
 namedWindow("Display Image", WINDOW_AUTOSIZE );
 imshow("Display Image", dst);
 waitKey(0);
